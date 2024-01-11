b196babccedddc61da795e8fdf3e278e43424491
Bowie <carlarhentschel@gmail.com>
2024-01-11 16:52:28 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+http.createServer((request, response) => {
+    
+    if(request.url === "/produto") {
+        response.end(JSON.stringify({
+            message: "Rota de produto",
+        }));
+    }
+
+    if(request.url === "/usuario") {
+        response.end(JSON.stringify({
+            message: "Rota de usuario",
+        }));
+    }
+
+
+    response.end(JSON.stringify({
+        message: "Qualquer outra rota",
+    }));
+
+}).listen(4001, () => console.log("Servidor está rodando na porta 4001"));
